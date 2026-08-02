96766fd0b1f9c5c02ae4d5c8ca233c79751d5377
rkWebCreater <jobnin.ryu7@gmail.com>
2026-08-02 16:33:57 +0900

    Add files via upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,17 @@ export default defineNuxtConfig({
     compilerOptions: {
       isCustomElement: (tag) => /^(swiper|swiper-slide|swiper-container)$/.test(tag),
     },
+  },
+  nitro: {
+    prerender: {
+      // 検索ページ（クエリパラメータ付き含む）の事前生成を対象外にする
+      ignore: [
+        '/search',
+        '/Search',
+        '/nomad-cafe-info/search',
+        '/nomad-cafe-info/Search'
+      ]
+    }
   }
 
 })

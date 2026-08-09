8d621911f085dc78b456800eda6bcf75e6f5ed67
rkWebCreater <jobnin.ryu7@gmail.com>
2026-08-09 09:30:40 +0900

    Add files via upload

--- components/CafeAreas.vue
+++ components/CafeAreas.vue
@@ -1,32 +1,36 @@
 <!-- カフェエリアのコンポーネント -->
+
 <script setup>
+
 //定数ファイルからAREASのリストを読み込む
 import {AREAS} from '~/constants/areas'
 
 </script> 
 <template>
  <div id="cafeArea" class="mt-20 mb-20">
-  
       <div class="ttl">
         <h2>エリアから探す</h2>
       </div>
       <ul class="area-list">
+
        <!-- 繰り返し処理 -->
        <NuxtLink v-for="area in AREAS" 
        :key="area.id" 
        :to="{path: '/search', query: { area: area.id }}" class="area-item">
-        <img :src="area.imageUrl" :alt="area.name" class="area-image" />
-        <p class="area-name">{{area.name}}</p>
-       </NuxtLink><!-- コロン（:）をつけることで、データの中の「imageUrl」の文字をそのまま画像パスとして正しく読み込みます。 -->  
+
+       <div class="area-image"><img :src="area.imageUrl" :alt="area.name"  /></div> 
+       
+       <div class="area-name"><p >{{area.name}}<br></p></div> 
+       
+      </NuxtLink><!-- コロン（:）をつけることで、データの中の「imageUrl」の文字をそのまま画像パスとして正しく読み込みます。 -->  
+
       </ul>
-  
   </div>
 </template>
 
 <style>
 /*PC,SP 共通 */
 #cafeArea{
-  background: rgb(253, 245, 238);
   padding:40px 0;
 
         .ttl{
@@ -48,22 +52,60 @@ import {AREAS} from '~/constants/areas'
 
     /* エリアボタン */
     .area-item{
+               width:stretch;
+               max-width:clamp(200px , 5vw , 300px);
+               height:70px;
                position: relative;
+               display: flex;
+               flex-wrap: wrap; 
+               
 
-      img{
-         border-radius: 10px;
-         position: absolute;
-         top: 50%;
-         left: 50%;
-         transform: translate(-50%, -50%);
+      .area-image{
+         width:clamp(150px , 4vw , 200px);
+         img{
+          border-radius: 10px;
+         }
+        
       }
-      p{
-        font-family:'zen kurenaido' ;
-        color:rgb(245, 230, 217) ;
-        background: rgb(104, 53, 4) ;
-        padding:5px ;
-        position: absolute ;
+      .area-name{
+              width:stretch ;
+              height: 70px ;
+              background: rgb(214, 200, 188);
+              border-radius: 10px;
+              /*右側を曲線にする */
+              clip-path: ellipse(100% 75% at 100% 50%);
+              position:absolute;
+              left:100px;
+            
+        p{
+          width: max-content;
+          color: rgb(104, 53, 4) ;
+          position:absolute;
+          top:50%;
+          left:57%;
+          transform : translate(-50% , -50%);
+         
+        }
+        p:after{
+           content:'→';
+           position:absolute;
+           left:50%;
+           transform:translateX(-50%);
+         
+        }
+        
       }
+      .area-name:before{
+              content:'';
+              width: 100%;
+              height: 70px;
+              background: rgb(255 247 240);
+              border-radius: 10px;
+              clip-path: ellipse(100% 75% at 100% 50%);
+              position: absolute;
+              left: 3px;
+
+            }
     }
     
 }

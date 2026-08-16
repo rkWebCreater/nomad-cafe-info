fbcf9cff28c701818a23c5ea03a7c9a51327bb7d
rkWebCreater <jobnin.ryu7@gmail.com>
2026-08-16 14:04:13 +0900

    Add files via upload

--- constants/tags.js
+++ constants/tags.js
@@ -1,15 +1,12 @@
-// タグのマスターデータ
-export const TAGS = [
-  { name: '電源', key: 'power' },
-  { name: 'Wifi', key: 'wifi' },
-  { name: 'モーニング', key: 'morning' },
-  { name: 'ランチ', key: 'lunch' },
-  { name: '一人席', key: 'single' },
-  { name: '禁煙', key: 'no-smoking' },
-  { name: '打あわせ可', key: 'meeting' }
-]
+import featureMaster from '../data/features.json'
+
+// タグのマスターデータ（features.json から自動生成）
+export const TAGS = Object.entries(featureMaster).map(([key, item]) => ({
+  name: item.name,
+  key: key
+}))
 
 // キーから日本語名へ素早く変換するための連想配列（Map）
 export const TAG_MAP = Object.fromEntries(
   TAGS.map(tag => [tag.key, tag.name])
-)
+)
